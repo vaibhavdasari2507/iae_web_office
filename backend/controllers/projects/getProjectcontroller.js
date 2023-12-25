9a26b8c4ea1ec67d4d7596adc669ddc2e4b1d41b
Vaibhav Dasari <83279965+vaibhavdasari2507@users.noreply.github.com>
2023-12-25 12:03:28 +0530

    Update getProjectcontroller.js

--- backend/controllers/projects/getProjectcontroller.js
+++ backend/controllers/projects/getProjectcontroller.js
@@ -1,34 +1,34 @@
 const User = require("../../models/userSchema");
-const redisClient = require("../../config/redis");
+// const redisClient = require("../../config/redis");
 
-async function getProjectDataFromCache(key) {
-    return new Promise((resolve, reject) => {
-        redisClient.get(key, (err, data) => {
-            if (err) {
-                reject(err);
-            } else if (!data) {
-                resolve(null);
-            } else {
-                resolve(JSON.parse(data));
-            }
-        });
-    });
-}
-function setProjectDataToCache(key, data) {
-    redisClient.set(key, JSON.stringify(data), 'EX', 3600);
-}
+// async function getProjectDataFromCache(key) {
+//     return new Promise((resolve, reject) => {
+//         redisClient.get(key, (err, data) => {
+//             if (err) {
+//                 reject(err);
+//             } else if (!data) {
+//                 resolve(null);
+//             } else {
+//                 resolve(JSON.parse(data));
+//             }
+//         });
+//     });
+// }
+// function setProjectDataToCache(key, data) {
+//     redisClient.set(key, JSON.stringify(data), 'EX', 3600);
+// }
 
 exports.getproject = async (req, res) => {
     try {
         const id = req.params.id.slice(1);
-        const cacheKey = `project:${id}`;
-        let projects = getProjectDataFromCache(cacheKey);
+        // const cacheKey = `project:${id}`;
+        // let projects = getProjectDataFromCache(cacheKey);
         if (!projects) {
             try {
                 const user = await User.findById({ _id: id }).populate("projects employees clients");
                 const projects = user.projects;
                 console.log(projects);
-                setProjectDataToCache(cacheKey, projects);
+                // setProjectDataToCache(cacheKey, projects);
                 res.status(200).json({ data: { projects }, message: "success" })
         
             } catch (error) {
