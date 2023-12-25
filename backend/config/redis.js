e96cddfb039717cecb4b62fa2ac54fdff5e52269
Vaibhav Dasari <83279965+vaibhavdasari2507@users.noreply.github.com>
2023-12-25 11:59:30 +0530

    first commit

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,19 +1,19 @@
-const redis = require('redis');
+// const redis = require('redis');
 
-const redisClient = redis.createClient({
-    password: 'bnTSp1ZibLguh0qPvZgu9L1ISE2ChUNs',
-    socket: {
-        host: 'redis-15257.c301.ap-south-1-1.ec2.cloud.redislabs.com',
-        port: 15257
-    }
-});
+// const redisClient = redis.createClient({
+//     password: 'bnTSp1ZibLguh0qPvZgu9L1ISE2ChUNs',
+//     socket: {
+//         host: 'redis-15257.c301.ap-south-1-1.ec2.cloud.redislabs.com',
+//         port: 15257
+//     }
+// });
 
-redisClient.connect()
-redisClient.on('connect', function() {
-    console.log('Redis client connected');
-});
-redisClient.on('error', function (err) {
-    console.log('Error: ' + err);
-});
+// redisClient.connect()
+// redisClient.on('connect', function() {
+//     console.log('Redis client connected');
+// });
+// redisClient.on('error', function (err) {
+//     console.log('Error: ' + err);
+// });
 
-module.exports =redisClient;
+// module.exports =redisClient;
